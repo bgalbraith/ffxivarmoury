5a741c2b6dbb28c0965f7def0a25eec94193475c
Byron Galbraith <byron.galbraith@gmail.com>
2011-10-10 03:48:09 -0400

    conversion to 960.gs for general layout one view for all event types

--- history/views.py
+++ history/views.py
@@ -28,86 +28,94 @@ def events(request, type_name, character_id):
     for group in groups:
         subs = [group]
         if group.eventgroup_set.count():
-            subs = group.eventgroup_set.all()
+            if type_name == 'quest':
+                nations = Nation.objects.exclude(name=character.nation.name)
+                exclude = ["Main Scenario - %s" % n.name for n in nations.all()]
+                subs = group.eventgroup_set.exclude(group__in=exclude)
+            elif type_name == 'guild' and group.group == 'General':
+                continue
+            else:
+                subs = group.eventgroup_set.all()
 
-        zone = {'id': group.id, 'name' : group.group, 'count' : 0, 'achieved' : 0, 'subs' : []}
+        if type_name == 'guild':
+            zone = {'id': group.id, 'name' : group.group, 'count' : 0, 'achieved' : 0, 'ranks' : [], 'quests' : [], 'purchases' : []}
+            abbr = group.guild.abbreviation.lower()
+        else:
+            zone = {'id': group.id, 'name' : group.group, 'count' : 0, 'achieved' : 0, 'subs' : []}
+
         for s in subs:
             sub = {'id' : s.id, 'name' : s.group, 'count' : s.event_set.count(), 'achieved' : 0, 'events' : []}
             for event in s.event_set.order_by('position').all():
-                label = re.sub(" reached!", "", event.title)
-                event_class = "event"
-                if label[0:4] == 'Camp' or label in ["Limsa Lominsa", "Ul'dah", "Gridania"]:
-                    event_class += " camp"
+                label = ''
+                event_class = ''
+                if type_name == 'quest':
+                    label = re.sub(" complete!", "", event.title)
+                    label = re.sub(r'^.+"(.+)".*$',r'\1',label)
+                    event_class = "quest-event"
+                elif type_name == 'guildleve':
+                    label = re.sub(" complete!", "", event.title)
+                    label = re.sub(r'^(.+) (earned|completed).*$',r'\1',label)
+                    event_class = "guildleve-event"
+                elif type_name == 'combat':
+                    label = re.sub(" defeated!", "", event.title)
+                    event_class = "combat-event"
+                elif type_name == 'special':
+                    label = re.sub(r" (obtained|complete)!", "", event.title)
+                    label = re.sub('!','',label)
+                    event_class = "special-event"
+                elif type_name == 'guild':
+                    if re.match(r'^.*Ranking$',s.group):
+                        label = re.sub(' achieved!','',event.title)
+                        event_class = abbr + '-rank'
+                    elif re.match(r'^.*Quests$',s.group):
+                        label = re.sub(' complete!','',event.title)
+                        event_class = 'guild-quest'
+                    elif re.match(r'^.*Purchases$',s.group):
+                        label = re.sub(' acquired!','',event.title)
+                        event_class = 'guild-purchase'
+                else:
+                    label = re.sub(" reached!", "", event.title)
+                    event_class = "event"
+                    if label[0:4] == 'Camp' or label in ["Limsa Lominsa", "Ul'dah", "Gridania"]:
+                        event_class += " camp"
                 if event in history:
                     event_class += " achieved"
                     sub['achieved'] += 1
-                sub['events'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
-            zone['count'] += sub['count']
-            zone['achieved'] += sub['achieved']
-            zone['subs'].append(sub)
-        events.append(zone)
-        total += zone['count']
-    return render_to_response('history/aetheryte.html', {
+                else:
+                    if type_name == 'special':
+                        continue
+
+                if type_name == 'guild':
+                    if re.match(r'^.*Ranking$',s.group):
+                        zone['ranks'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
+                    elif re.match(r'^.*Quests$',s.group):
+                        zone['quests'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
+                    elif re.match(r'^.*Purchases$',s.group):
+                        zone['purchases'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
+                else:
+                    sub['events'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
+            
+            if sub['achieved'] > 0 or type_name != "special":
+                zone['count'] += sub['count']
+                zone['achieved'] += sub['achieved']
+                if type_name != 'guild':
+                    zone['subs'].append(sub)
+
+        if zone['achieved'] > 0 or type_name != "special":
+            events.append(zone)
+            total += zone['count']
+    return render_to_response('history/%s.html' % type_name, {
         "events" : events,
         "character" : character,
         "count" : count,
         "total" : total,
         "characters" : Character.objects.all(),
         "types" : EventType.objects.all(),
-        "type" : 'aetheryte'})
-
-
-def aetheryte(request, characterId):
-    try:
-        character = Character.objects.get(pk=characterId)
-    except Character.DoesNotExist:
-        raise Http404
-    groups = EventGroup.objects.filter(type=1, parent=0)
-    history = character.events.filter(group__type=1)
-    count = history.count()
-    total = 0
-    events = []
-    for group in groups:
-        subs = [group]
-        if group.eventgroup_set.count():
-            subs = group.eventgroup_set.all()
-
-        zone = {'id': group.id, 'name' : group.group, 'count' : 0, 'achieved' : 0, 'subs' : []}
-        for s in subs:
-            sub = {'id' : s.id, 'name' : s.group, 'count' : s.event_set.count(), 'achieved' : 0, 'events' : []}
-            for event in s.event_set.order_by('position').all():
-                label = re.sub(" reached!", "", event.title)
-                event_class = "event"
-                if label[0:4] == 'Camp' or label in ["Limsa Lominsa", "Ul'dah", "Gridania"]:
-                    event_class += " camp"
-                if event in history:
-                    event_class += " achieved"
-                    sub['achieved'] += 1
-                sub['events'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
-            zone['count'] += sub['count']
-            zone['achieved'] += sub['achieved']
-            zone['subs'].append(sub)
-        events.append(zone)
-        total += zone['count']
-    return render_to_response('history/aetheryte.html', {
-        "events" : events,
-        "character" : character,
-        "count" : count,
-        "total" : total,
-        "characters" : Character.objects.all(),
-        "types" : EventType.objects.all(),
-        "type" : 'aetheryte'})
+        "type" : type_name})
 
 def guild(request, characterId):
-    try:
-        character = Character.objects.get(pk=characterId)
-    except Character.DoesNotExist:
-        raise Http404
-    groups = EventGroup.objects.filter(type=2, parent=0).exclude(group='General')
-    history = character.events.filter(group__type=2)
-    count = history.count()
-    total = 0
-    guilds = []
+    #groups = EventGroup.objects.filter(type=2, parent=0).exclude(group='General')
+
 
     ## every guild group (except general) has a rank, quest, and purchase subgroup
     ## general has phys and sentinel ranks and war/magic purchases
@@ -152,131 +160,3 @@ def guild(request, characterId):
         total += guild['count']
         guilds.append(guild)
     return render_to_response('history/guild.html', { "guilds" : guilds, "character" : character, "count" : count, "total" : total })
-
-def quest(request, characterId):
-    try:
-        character = Character.objects.get(pk=characterId)
-    except:
-        raise Http404
-    nations = Nation.objects.exclude(name=character.nation.name)
-    exclude = ["Main Scenario - %s" % n.name for n in nations.all()]
-    groups = EventGroup.objects.filter(type=3, parent=0)
-    history = character.events.filter(group__type=3)
-    count = history.count()
-    total = 0
-    events = []
-    for group in groups:
-        subs = [group]
-        if group.eventgroup_set.count():
-            subs = group.eventgroup_set.exclude(group__in=exclude)
-
-        zone = {'id': group.id, 'name' : group.group, 'count' : 0, 'achieved' : 0, 'subs' : []}
-        for s in subs:
-            sub = {'id' : s.id, 'name' : s.group, 'count' : s.event_set.count(), 'achieved' : 0, 'events' : []}
-            for event in s.event_set.order_by('position').all():
-                label = re.sub(" complete!", "", event.title)
-                label = re.sub(r'^.+"(.+)".*$',r'\1',label)
-                event_class = "quest-event"
-                if event in history:
-                    event_class += " achieved"
-                    sub['achieved'] += 1
-                sub['events'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
-            zone['count'] += sub['count']
-            zone['achieved'] += sub['achieved']
-            zone['subs'].append(sub)
-        events.append(zone)
-        total += zone['count']
-    return render_to_response('history/quest.html', { "events" : events, "character" : character, "count" : count, "total" : total })
-    
-def guildleve(request, characterId):
-    try:
-        character = Character.objects.get(pk=characterId)
-    except:
-        raise Http404
-    groups = EventGroup.objects.filter(type=4, parent=0)
-    history = character.events.filter(group__type=4)
-    count = history.count()
-    total = 0
-    events = []
-    for group in groups:
-        subs = [group]
-        if group.eventgroup_set.count():
-            subs = group.eventgroup_set.all()
-
-        zone = {'id': group.id, 'name' : group.group, 'count' : 0, 'achieved' : 0, 'subs' : []}
-        for s in subs:
-            sub = {'id' : s.id, 'name' : s.group, 'count' : s.event_set.count(), 'achieved' : 0, 'events' : []}
-            for event in s.event_set.order_by('position').all():
-                label = re.sub(" complete!", "", event.title)
-                label = re.sub(r'^(.+) (earned|completed).*$',r'\1',label)
-                event_class = "guildleve-event"
-                if event in history:
-                    event_class += " achieved"
-                    sub['achieved'] += 1
-                sub['events'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
-            zone['count'] += sub['count']
-            zone['achieved'] += sub['achieved']
-            zone['subs'].append(sub)
-        events.append(zone)
-        total += zone['count']
-    return render_to_response('history/guildleve.html', { "events" : events, "character" : character, "count" : count, "total" : total })
-
-def combat(request, characterId):
-    try:
-        character = Character.objects.get(pk=characterId)
-    except:
-        raise Http404
-    groups = EventGroup.objects.filter(type=5, parent=0)
-    history = character.events.filter(group__type=5)
-    count = history.count()
-    total = 0
-    events = []
-    for group in groups:
-        subs = [group]
-        if group.eventgroup_set.count():
-            subs = group.eventgroup_set.all()
-
-        zone = {'id': group.id, 'name' : group.group, 'count' : 0, 'achieved' : 0, 'subs' : []}
-        for s in subs:
-            sub = {'id' : s.id, 'name' : s.group, 'count' : s.event_set.count(), 'achieved' : 0, 'events' : []}
-            for event in s.event_set.order_by('position').all():
-                label = re.sub(" defeated!", "", event.title)
-                event_class = "combat-event"
-                if event in history:
-                    event_class += " achieved"
-                    sub['achieved'] += 1
-                sub['events'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
-            zone['count'] += sub['count']
-            zone['achieved'] += sub['achieved']
-            zone['subs'].append(sub)
-        events.append(zone)
-        total += zone['count']
-    return render_to_response('history/combat.html', { "events" : events, "character" : character, "count" : count, "total" : total })
-
-def special(request, characterId):
-    try:
-        character = Character.objects.get(pk=characterId)
-    except:
-        raise Http404
-    groups = EventGroup.objects.filter(type=6, parent=0)
-    history = character.events.filter(group__type=6)
-    events = []
-    for group in groups:
-        subs = [group]
-        if group.eventgroup_set.count():
-            subs = group.eventgroup_set.all()
-
-        zone = {'id': group.id, 'name' : group.group, 'subs' : []}
-        for s in subs:
-            sub = {'id' : s.id, 'name' : s.group, 'events' : []}
-            for event in s.event_set.order_by('position').all():
-                label = re.sub(r" (obtained|complete)!", "", event.title)
-                label = re.sub('!','',label)
-                event_class = "special-event"
-                if event in history:
-                    sub['events'].append({'id' : event.id, 'label' : label, 'event_class' : event_class})
-            if len(sub['events']):
-                zone['subs'].append(sub)
-        if len(zone['subs']):
-            events.append(zone)
-    return render_to_response('history/special.html', { "events" : events, "character" : character })
